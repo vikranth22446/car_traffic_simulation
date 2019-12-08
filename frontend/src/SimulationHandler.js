fcdaaff30e0a09505254e8e3d2d576ea20760a59
Vikranth Srivatsa <rama22446@gmail.com>
2019-12-08 01:28:11 -0800

    update prod gcp

--- frontend/src/SimulationHandler.js
+++ frontend/src/SimulationHandler.js
@@ -289,8 +289,8 @@ class SimulationHandler extends Component {
         // establish websocket connection to backend server.
         var environment = process.env.NODE_ENV === 'production' ? 'production' : 'development';
         // Had to manually add this because https://github.com/parcel-bundler/parcel/issues/496#issuecomment-366993459
-        let ws = new WebSocket('ws://localhost:5000/ws');
-
+        // let ws = new WebSocket('ws://localhost:5000/ws');
+        let ws = new WebSocket('ws://35.202.208.248:5000/ws')
         // create and assign a socket to a variable.
         let socket = this.socket = new Socket(ws);
 
